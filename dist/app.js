5d6645684b205f0b3bca92cfcf5fdcc48fa20bc7
Blaze <sandevabeykoon123@gmail.com>
2021-05-29 18:02:36 +0530

    update:(procfile) change port

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 9000;
+const port = 0.0.0.0.;
 
 app.use(express.static(path.join(__dirname, "/")));
 

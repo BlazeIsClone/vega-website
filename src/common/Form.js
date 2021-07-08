dfc81b77e842cbfd08c7b0b62d6d608178022303
Blaze <sandevabeykoon123@gmail.com>
2021-07-08 22:51:02 +0530

    minor:(Form.js) method fix

--- src/common/Form.js
+++ src/common/Form.js
@@ -38,7 +38,7 @@ function Form(props) {
             />
           </HeadlineWrapper>
           {/* ---- Name ---- */}
-          <form method="post" id="form-enquiry">
+          <form method="POST" id="form-enquiry">
             <FormBlock>
               <FormElementName>
                 <InputLabel>Name</InputLabel>
